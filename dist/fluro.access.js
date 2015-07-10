09261c6b57cbc722774b611a80e2f56485638aff
Cade <cade@hyperblaster.org>
2015-07-10 16:35:22 +1000

    First commit

---
+++ dist/fluro.access.js
@@ -0,0 +1,451 @@
+
+//Create Fluro UI With dependencies
+angular.module('fluro.access', [
+	'fluro.content'
+]);
+angular.module('fluro.access')
+.service('FluroAccess', function($rootScope, $q, FluroContent) {
+
+    var controller = {}
+
+    /////////////////////////////////////////////////////
+
+    controller.isFluroAdmin = function() {
+        return ($rootScope.user.accountType == 'administrator');
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    controller.getPermissionSets = function() {
+        return $rootScope.user.permissionSets;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+
+    controller.has = function(permission) {
+        var permissionSets = $rootScope.user.permissionSets;
+
+        var permissions = _.chain(permissionSets)
+        .map(function(permissionSet) {
+            return permissionSet.permissions;
+        })
+        .flatten()
+        .uniq()
+        .value();
+
+        console.log('Has Permissions', permissions);
+        return _.contains(permissions, permission);
+
+    }
+
+    /////////////////////////////////////////////////////
+
+    controller.canAccess = function(type) {
+
+        if (controller.isFluroAdmin()) {
+            return true;
+        }
+
+        //Check if the user has access to a few realms
+        var canCreateRealms = controller.retrieveActionableRealms('create ' + type);
+
+        //Can view this type
+        var canViewOwnRealms = controller.retrieveActionableRealms('view own ' + type);
+        var canViewAnyRealms = controller.retrieveActionableRealms('view any ' + type);
+
+        //Can edit this type
+        var canEditOwnRealms = controller.retrieveActionableRealms('edit own ' + type);
+        var canEditAnyRealms = controller.retrieveActionableRealms('edit any ' + type);
+
+        //Get all the realms
+        var totalRealms = [];
+        totalRealms = totalRealms.concat(canViewOwnRealms);
+        totalRealms = totalRealms.concat(canViewAnyRealms);
+        totalRealms = totalRealms.concat(canEditOwnRealms);
+        totalRealms = totalRealms.concat(canEditAnyRealms);
+
+        if (totalRealms.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /////////////////////////////////////////////////////
+
+    controller.retrieveActionableRealms = function(action) {
+
+        /////////////////////////////////
+
+        //Get the permission sets
+        var permissionSets = $rootScope.user.permissionSets;
+
+        //Find all realms we can view any of this type
+        return _.chain(permissionSets)
+            .map(function(realmSet, key) {
+
+                var searchString = action;
+                if (_.contains(realmSet.permissions, searchString)) {
+                    return key.toString();
+                }
+            })
+            .compact()
+            .value();
+    }
+
+    /////////////////////////////////////////////////////
+
+    controller.retrieveSelectableRealms = function(action) {
+
+        var realms;
+
+        if (controller.isFluroAdmin()) {
+            realms = FluroContent.resource('realm').query({
+                list: true
+            });
+        } else {
+
+            //Get the permission sets
+            var permissionSets = $rootScope.user.permissionSets;
+
+            //Find all realms we can view any of this type
+            realms = _.chain(permissionSets)
+                .filter(function(realmSet, key) {
+                    var searchString = action;
+                    return _.contains(realmSet.permissions, searchString);
+                })
+                .compact()
+                .map(function(realm) {
+                    return {
+                        _id: realm._id,
+                        title: realm.title,
+                    }
+                })
+                .value();
+        }
+
+        /////////////////////////////////
+
+        return realms;
+
+    }
+
+    /////////////////////////////////////////////////////
+
+    //Function to check if this user has this permission
+    controller.can = function(action, type) {
+
+        if (controller.isFluroAdmin()) {
+            return true;
+        }
+
+        //Get the permission string
+        var perm = action + ' ' + type;
+
+        /////////////////////////////////////////////////////
+
+        //Track the realms we are allowed to do this in
+        var realms = [];
+
+        /////////////////////////////////////////////////////
+
+        switch (action) {
+            case 'view any':
+                var canViewAnyRealms = controller.retrieveActionableRealms('view any ' + type);
+                var canEditAnyRealms = controller.retrieveActionableRealms('edit any ' + type);
+
+                //Combine the realms
+                realms = realms.concat(canViewAnyRealms);
+                realms = realms.concat(canEditAnyRealms);
+                break;
+            case 'view own':
+                var canViewOwnRealms = controller.retrieveActionableRealms('view own ' + type);
+                var canEditOwnRealms = controller.retrieveActionableRealms('edit own ' + type);
+
+                //Combine the realms
+                realms = realms.concat(canViewOwnRealms);
+                realms = realms.concat(canEditOwnRealms);
+                break;
+            default:
+                realms = controller.retrieveActionableRealms(perm);
+                break;
+        }
+
+        if (realms.length) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /////////////////////////////////////////////////////
+
+    controller.isAuthor = function(item) {
+        //Check if the user is the author
+        var author = false;
+
+        //Only allow if author of the content
+        if (_.isObject(item.author)) {
+            author = (item.author._id == $rootScope.user._id);
+        } else {
+            author = (item.author == $rootScope.user._id);
+        }
+
+        ////////////////////////////////////////
+
+        if ($rootScope.user._id == item._id) {
+            //We are looking at our own profile
+            author = true;
+        }
+
+        return author;
+    }
+
+    /////////////////////////////////////////////////////
+
+    controller.canEditItem = function(item, user) {
+
+
+        if (controller.isFluroAdmin()) {
+            return true;
+        }
+
+        if(!item) {
+            return false;
+        }
+
+
+
+        var definitionName = item._type;
+
+        if (item.definition) {
+            definitionName = item.definition;
+        }
+
+        ////////////////////////////////////////
+
+        //Check if the user is the author of this content
+        var author = controller.isAuthor(item);
+
+        if (user) {
+            definitionName = 'user';
+
+            if (author) {
+                return true;
+            }
+        }
+
+        ////////////////////////////////////////
+
+
+        //Get the realms we are allowed to work in
+        var editAnyRealms = controller.retrieveActionableRealms('edit any ' + definitionName);
+        var editOwnRealms = controller.retrieveActionableRealms('edit own ' + definitionName);
+
+        var contentRealmIds = _.map(item.realms, function(realm) {
+            if (realm._id) {
+                return realm._id;
+            }
+            return realm;
+        });
+
+        if(!item.realms) {
+            //No realms associated with this content
+        }
+
+        ////////////////////////////////////////
+
+        //Find any matches between this content
+        var matchedAnyRealms = _.intersection(editAnyRealms, contentRealmIds);
+
+        //We are allowed to edit anything in these realms
+        //So return true
+        if (matchedAnyRealms.length) {
+            return true;
+        }
+
+
+        ////////////////////////////////////////
+
+        //If we are the author
+        if (author) {
+            //Find own matches between this content
+            var matchedOwnRealms = _.intersection(editOwnRealms, contentRealmIds);
+
+            //We are allowed to edit anything in these realms
+            //So return true
+            if (matchedOwnRealms.length) {
+                return true;
+            }
+        }
+    }
+
+
+    /////////////////////////////////////////////////////
+
+    controller.canViewItem = function(item, user) {
+
+        if (controller.isFluroAdmin()) {
+            return true;
+        }
+
+        var definitionName = item._type;
+
+        if (item.definition) {
+            definitionName = item.definition;
+        }
+
+        ////////////////////////////////////////
+        //Check if the user is the author of this content
+        var author = controller.isAuthor(item);
+
+        if (user) {
+            definitionName = 'user';
+
+            if (author) {
+                return true;
+            }
+        }
+
+        ////////////////////////////////////////
+
+        //Get the realms we are allowed to work in
+        var viewAnyRealms = controller.retrieveActionableRealms('view any ' + definitionName);
+        var viewOwnRealms = controller.retrieveActionableRealms('view own ' + definitionName);
+        var editAnyRealms = controller.retrieveActionableRealms('edit any ' + definitionName);
+        var editOwnRealms = controller.retrieveActionableRealms('edit own ' + definitionName);
+
+        //Combine any
+        var combinedAnyRealms = [];
+        combinedAnyRealms = combinedAnyRealms.concat(viewAnyRealms);
+        combinedAnyRealms = combinedAnyRealms.concat(editAnyRealms);
+
+        //Combine own
+        var combinedOwnRealms = [];
+        combinedOwnRealms = combinedOwnRealms.concat(viewOwnRealms);
+        combinedOwnRealms = combinedOwnRealms.concat(editOwnRealms);
+
+        ////////////////////////////////////////
+
+        var contentRealmIds = _.map(item.realms, function(realm) {
+            if (realm._id) {
+                return realm._id;
+            }
+            return realm;
+        });
+
+        ////////////////////////////////////////
+
+        //Find any matches between this content
+        var matchedAnyRealms = _.intersection(combinedAnyRealms, contentRealmIds);
+
+        //We are allowed to edit anything in these realms
+        //So return true
+        if (matchedAnyRealms.length) {
+            return true;
+        }
+
+        ////////////////////////////////////////
+
+        //If we are the author
+        if (author) {
+            //Find own matches between this content
+            var matchedOwnRealms = _.intersection(combinedOwnRealms, contentRealmIds);
+
+            //We are allowed to edit anything in these realms
+            //So return true
+            if (matchedOwnRealms.length) {
+                return true;
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////
+
+    controller.canDeleteItem = function(item, user) {
+
+        if (controller.isFluroAdmin()) {
+            return true;
+        }
+
+        var definitionName = item._type;
+
+        if (item.definition) {
+            definitionName = item.definition;
+        }
+
+        ////////////////////////////////////////
+        //Check if the user is the author of this content
+        var author = controller.isAuthor(item);
+
+        if (user) {
+            definitionName = 'user';
+
+            if (author) {
+                return true;
+            }
+        }
+
+        ////////////////////////////////////////
+
+        //Get the realms we are allowed to work in
+        var deleteAnyRealms = controller.retrieveActionableRealms('delete any ' + definitionName);
+        var deleteOwnRealms = controller.retrieveActionableRealms('delete own ' + definitionName);
+
+        var contentRealmIds = _.map(item.realms, function(realm) {
+            if(realm) {
+                if (realm._id) {
+                    return realm._id;
+                }
+            }
+            return realm;
+        });
+
+        ////////////////////////////////////////
+
+        //Find any matches between this content
+        var matchedAnyRealms = _.intersection(deleteAnyRealms, contentRealmIds);
+
+        //We are allowed to delete anything in these realms
+        //So return true
+        if (matchedAnyRealms.length) {
+            return true;
+        }
+
+        ////////////////////////////////////////
+
+        //If we are the author
+        if (author) {
+            //Find own matches between this content
+            var matchedOwnRealms = _.intersection(deleteOwnRealms, contentRealmIds);
+
+            //We are allowed to delete anything in these realms
+            //So return true
+            if (matchedOwnRealms.length) {
+                return true;
+            }
+        }
+    }
+
+
+    /////////////////////////////////////////////////////
+
+    controller.resolveIf = function(bool) {
+        var deferred = $q.defer();
+
+        if (bool) {
+            deferred.resolve()
+        } else {
+            // MessageService.post('Access denied', 'error');
+            deferred.reject();
+        }
+
+        return deferred.promise;
+    }
+
+    /////////////////////////////////////////////////////
+
+    return controller;
+
+
+});
